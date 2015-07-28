61379c01cbeac82ff991306bce4d7de4c264418f
Dan Lindeman <lindemda@gmail.com>
2015-07-28 14:37:21 -0400

    change get to post

--- pingpong.rb
+++ pingpong.rb
@@ -9,9 +9,9 @@ AUTH = { username: conf['username'], password: conf['password'] }
 
 def pong
   headers = { 'Content-Type' => 'application/json' }
-  body = JSON.dump({ key: 'value', 
-                     key1: 'value1', 
-                     key2: 'value2', 
+  body = JSON.dump({ key: 'value',
+                     key1: 'value1',
+                     key2: 'value2',
                      key3: 'value3' })
   options = { headers: headers, body: body, basic_auth: AUTH,  verify: false }
   results = HTTParty.post(BASE_URI, options)
@@ -19,7 +19,6 @@ end
 
 pong if conf['initiator'] == true
 
-get '/ping' do
+post '/ping' do
   pong
 end
-
