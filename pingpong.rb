40d335a565103ed71df10c3ffe1c30a0436b24eb
Dan Lindeman <lindemda@gmail.com>
2015-08-07 15:12:04 -0400

    rockets do not work

--- pingpong.rb
+++ pingpong.rb
@@ -8,10 +8,10 @@ AUTH = { username: conf['username'], password: conf['password'] }
 
 def pong
   headers = { 'Content-Type' => 'application/json' }
-  body = JSON.dump({ key: =>'value',
-                     key1: => 'value1',
-                     key2: => 'value2',
-                     key3: => 'value3' })
+  body = JSON.dump({ key: 'value',
+                     key1: 'value1',
+                     key2: 'value2',
+                     key3: 'value3' })
   options = { headers: headers, body: body, basic_auth: AUTH,  verify: false }
   begin
     results = HTTParty.post(BASE_URI, options)
